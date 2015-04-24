1f655afbe25ee7d502e380e481754ab317f0be90
colorfullyme <ask.colorfully.me@gmail.com>
2015-04-24 10:58:26 +0300

    Fixes security holes for socket connection and not found parameters

--- controllers/socket.js
+++ controllers/socket.js
@@ -37,6 +37,7 @@ function getAllAPIs() {
 
 /**
  * Event handler for `getAPIs` call.
+ *
  * Emits the available APIs for the spin wheel.
  * With those the wheel will be created on client side.
  *
@@ -48,7 +49,8 @@ function getAPIs(socket, user, pathname) {
 
   Event.findById(parameters.eventId, function (err, event) {
     if (err) {
-      socket.emit('error', err);
+      socket.emit('_error_', err);
+      return;
     }
     // find the team
     var team = _.find(event.teams, {
@@ -73,69 +75,86 @@ function getAPIs(socket, user, pathname) {
  * @param {Object} user
  * @param {Object} options
  */
-function spin(socket, user, options) {
+function spin(socket, user, pathname) {
   var startAngle = 0;
   var spinTime = 0;
   var spinTimeTotal = Math.random() * 3 * 1000;
   var spinInterval = 10;
-  var parameters = parsePath(options.pathname);
+  var parameters = parsePath(pathname);
+  var APIs = getAllAPIs();
+  var arc = Math.PI / (APIs.length / 2);
 
-  // Helper to start spinning...
-  function spinme() {
-    spinTime += spinInterval;
-    if (spinTime >= spinTimeTotal) {
-      stopRotateWheel();
+  // Get event and team data from db.
+  Event.findById(parameters.eventId, function (err, event) {
+    if (err || !event) {
+      socket.emit('_error_', err || 'Event does not exists');
       return;
     }
-    var spinAngleStart = Math.random() * 10 + 10;
-    var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
-    startAngle += (spinAngle * Math.PI / 180);
-
-    socket.emit('spinning', startAngle);
-    setTimeout(spinme, spinInterval);
-  }
-
-  // Helper to stop the rotation.
-  function stopRotateWheel() {
-    var degrees = startAngle * 180 / Math.PI + 90;
-    var arcd = options.arc * 180 / Math.PI;
-    var index = Math.floor((360 - degrees % 360) / arcd);
-
-    // Write the result to db.
-    Event.findById(parameters.eventId, function (err, event) {
-      if (err) {
-        socket.emit('error', err);
-      }
-      // find the team index
-      var teamIndex = _.findIndex(event.teams, {
-        id: parameters.teamId
-      });
-
-      // Chehck if API is already added?
-      if (_.indexOf(event.teams[teamIndex].apiSpins, options.APIs[index]) != -1) {
-        socket.emit('spin result', 'present');
-      } else {
-        event.teams[teamIndex].apiSpins.push(options.APIs[index]);
-        event.save(function (err) {
-          if (err) {
-            socket.emit('error', err);
-          } else {
-            socket.emit('spin result', options.APIs[index]);
-          }
-        });
-      }
+    // find the team index
+    var teamIndex = _.findIndex(event.teams, {
+      id: parameters.teamId
     });
-  }
+    if(teamIndex == -1){
+      socket.emit('_error_', 'Team does not exists');
+      return;
+    }
 
-  // Helper to ease out rotation.
-  function easeOut(t, b, c, d) {
-    var ts = (t /= d) * t;
-    var tc = ts * t;
-    return b + c * (tc + -3 * ts + 3 * t);
-  }
+    var apiSpins = event.teams[teamIndex].apiSpins;
+    var allowedSpins = process.env.NUMBER_OF_SPINS || 3;
 
-  // Start spinning...
-  spinme();
+    // Make sure only teamleader can spin and there are spins left.
+    if(event.teams[teamIndex].leader === user.member.handle && apiSpins.length < allowedSpins){
+
+      // Helper to start spinning...
+      function spinme() {
+        spinTime += spinInterval;
+        if (spinTime >= spinTimeTotal) {
+          stopRotateWheel();
+          return;
+        }
+        var spinAngleStart = Math.random() * 10 + 10;
+        var spinAngle = spinAngleStart - easeOut(spinTime, 0, spinAngleStart, spinTimeTotal);
+        startAngle += (spinAngle * Math.PI / 180);
+
+        socket.emit('spinning', startAngle);
+        setTimeout(spinme, spinInterval);
+      }
+
+      // Helper to stop the rotation.
+      function stopRotateWheel() {
+        var degrees = startAngle * 180 / Math.PI + 90;
+        var arcd = arc * 180 / Math.PI;
+        var index = Math.floor((360 - degrees % 360) / arcd);
+
+        // Chehck if API is already added?
+        if (_.indexOf(event.teams[teamIndex].apiSpins, APIs[index]) != -1) {
+          socket.emit('spin result', 'present');
+        } else {
+          event.teams[teamIndex].apiSpins.push(APIs[index]);
+          event.save(function (err) {
+            if (err) {
+              socket.emit('_error_', err);
+            } else {
+              socket.emit('spin result', APIs[index]);
+            }
+          });
+        }
+      }
+
+      // Helper to ease out rotation.
+      function easeOut(t, b, c, d) {
+        var ts = (t /= d) * t;
+        var tc = ts * t;
+        return b + c * (tc + -3 * ts + 3 * t);
+      }
+
+      // Start spinning...
+      spinme();
+
+    }else{
+      socket.emit('_error_', 'You are not allowed to spin the wheel');
+    }
+  });
 }
 
 module.exports = function (socket, user) {
